129b8027934474e7e64a2054fb7c982601492bd5
yutteee <yusaku_nakamura_aa@icloud.com>
2024-06-08 22:57:59 +0900

    first init

---
+++ ai-tango/WordRow.swift
@@ -0,0 +1,18 @@
+//
+//  WordRow.swift
+//  ai-tango
+//
+//  Created by 中村優作 on 2024/06/08.
+//
+
+import SwiftUI
+
+struct WordRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WordRow()
+}
---
+++ ai-tango/word.swift
@@ -0,0 +1,8 @@
+//
+//  word.swift
+//  ai-tango
+//
+//  Created by 中村優作 on 2024/06/08.
+//
+
+import Foundation
